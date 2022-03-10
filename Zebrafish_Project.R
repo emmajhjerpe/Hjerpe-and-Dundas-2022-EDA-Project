4d08a93d8711310b68cda076f35f3a2069d57833
Jordan Dundas <dundas.jordan21@gmail.com>
2022-03-10 11:38:44 -0600

    Renamed Variables

--- Zebrafish_Project.R
+++ Zebrafish_Project.R
@@ -1,5 +1,17 @@
-library(tidyverse)
-library("readxl")
+library(tidyverse, ggplot2, readxl)
 # xlsx files
 my_data <- read_excel( file.choose("ZebrafishQuantData.xlsx"))
 view(my_data)                      
+zebrafish_data<-rename(my_data, total_length = TL, 
+              yolk_height = YolkHt, tail_length = "Lttailfromtipmsc(mm)", 
+            body_cavity_lenth = BodyCavLen, trunk_total = TrunkTotal, 
+            head_depth = HeadDpth, caudal_fin_height = CaudHt, 
+            trunk_and_fin = "Trunk+Fin", dorsal_fin_height = DorsalFin, 
+            dorsal_fin_length = DorsalFinLen, anal_fin_length = AnalFinLen, eye_height = EyeHt, 
+            eye_length = EyeLen, hatch_time_hours = "hatchtime(hours)", average_velocity = VelAvg, 
+            max_velocity = VelMax)
+view(zebrafish_data)
+ggplot(data = my_data) + 
+  geom_point(mapping = aes(x = TL, y = YolkHt, color = hatchtime(hours), shape = W1A2)
+
+             
